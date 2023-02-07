363995d42a5aa706485d6a2257955421a649e04f
JuliaStella <julia.paula@liferay.com>
2023-02-07 17:11:05 -0300

    codigo de fyfood

---
+++ fyfood.py
@@ -0,0 +1,5 @@
+matriz = ['0','0','0','R','0','0','A','0','B','0','0','0','0','0','0','0','0','C','0','D','0']
+#Ajustar a posicao dos zeros
+def fyfood(matriz):
+    if matriz[0] == 0:
+         return "R"
