5169ab1713ce98738d6f248b9ae6c596071fc4db
JuliaStella <julia.paula@liferay.com>
2023-02-08 13:51:12 -0300

    Primeira parte da permutacao

--- fyfood.py
+++ fyfood.py
@@ -1,5 +1,21 @@
-matriz = ['0','0','0','R','0','0','A','0','B','0','0','0','0','0','0','0','0','C','0','D','0']
-#Ajustar a posicao dos zeros
-def fyfood(matriz):
-    if matriz[0] == 0:
-         return "R"
+# Criar arquivo para leitura;
+# - Leitura do arquivo;
+# Criar permutacatacao;
+# Criar soma das distancias;
+# Plano cartesiano visualmente com o codigo;
+# Criar grafico de tempo com representacao grafica;
+
+
+#Permutacao:
+def per(lista):
+    if len(lista) <= 1:
+        return [lista]
+    lista_sobra = []
+    for i, atual in enumerate(lista):
+        elementos_restantes = lista[:i] + lista[i+1:]
+        for diminuicao in per(elementos_restantes):
+            lista_sobra.append([atual]+diminuicao)
+    return lista_sobra
+
+pontos = [1, 2, 3, 4]
+print(per(pontos))
